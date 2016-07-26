40f53dda88a1c9d97bac7c685f458e8f074df4ec
vmyra <vitormyra@outlook.com>
2016-07-26 14:07:05 -0300

    cores do lado esquerdo da informação no listview

--- AgendaContato/app/src/main/java/br/com/videoaula/agendacontato/ContatoArrayAdapter.java
+++ AgendaContato/app/src/main/java/br/com/videoaula/agendacontato/ContatoArrayAdapter.java
@@ -18,11 +18,13 @@ public class ContatoArrayAdapter extends ArrayAdapter<Contato> {
 
     private int resource = 0;
     private LayoutInflater inflater;
+    private Context context;
 
-    public ContatoArrayAdapter(Context context, int resource){
+    public ContatoArrayAdapter(Context context, int resource) {
         super(context, resource);
-        inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         this.resource = resource;
+        this.context = context;
     }
 
     @NonNull
@@ -30,34 +32,42 @@ public class ContatoArrayAdapter extends ArrayAdapter<Contato> {
     public View getView(int position, View convertView, ViewGroup parent) {
         View view = null;
         ViewHolder viewHolder = null;
-        if(convertView == null){
+        if (convertView == null) {
 
             viewHolder = new ViewHolder();
 
             view = inflater.inflate(resource, parent, false);
 
-            viewHolder.txtCor = (TextView)view.findViewById(R.id.txtCor);
-            viewHolder.txtNome = (TextView)view.findViewById(R.id.txtNome);
-            viewHolder.txtTelefone = (TextView)view.findViewById(R.id.txtTelefone);
+            viewHolder.txtCor = (TextView) view.findViewById(R.id.txtCor);
+            viewHolder.txtNome = (TextView) view.findViewById(R.id.txtNome);
+            viewHolder.txtTelefone = (TextView) view.findViewById(R.id.txtTelefone);
 
             view.setTag(viewHolder);
 
             convertView = view;
-        }
-        else{
-            viewHolder = (ViewHolder)convertView.getTag();
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
             view = convertView;
         }
 
         Contato contato = getItem(position);
-        //viewHolder.txtCor
+
+        if (contato.getNome().toUpperCase().startsWith("A")) {
+            viewHolder.txtCor.setBackgroundColor(context.getResources().getColor(R.color.azul));
+        }
+        else  if (contato.getNome().toUpperCase().startsWith("B")) {
+            viewHolder.txtCor.setBackgroundColor(context.getResources().getColor(R.color.vermelho));
+        }
+        else {
+            viewHolder.txtCor.setBackgroundColor(context.getResources().getColor(R.color.colorPrimaryDark));
+        }
         viewHolder.txtNome.setText(contato.getNome());
         viewHolder.txtTelefone.setText(contato.getTelefone());
 
         return view;
     }
 
-    static class ViewHolder{
+    static class ViewHolder {
         TextView txtCor;
         TextView txtNome;
         TextView txtTelefone;
